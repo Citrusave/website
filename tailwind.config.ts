de49fa5005fe0e374f2c280bafc73cbbc55e7723
yolocat <81561018+yolocat-dev@users.noreply.github.com>
2023-12-05 12:55:58 +0100

    feat: initial design (#2)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,24 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}'
-  ],
-  theme: {},
-  plugins: [],
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}'
+	],
+	theme: {
+		extend: {
+			colors: {
+				citrus: '#EE6C4D',
+				theme: {
+					'100': '#E0FBFC',
+					'300': '#98C1D9',
+					'600': '#3D5A80',
+					'800': '#293241'
+				}
+			}
+		}
+	},
+	plugins: [],
 };
 
 export default config;
